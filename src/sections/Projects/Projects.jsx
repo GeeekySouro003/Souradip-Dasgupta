463f954c0c9e08706b891e159cf3d571ebc176fc
Souradip <dgsouro77@gmail.com>
2024-07-02 01:56:20 +0530

    Added the projects to the section with link

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,18 @@
 import styles from '../Projects/ProjectsStyles.module.css';
 import ChatNet from  '../../assets/chatnet.png';
+import InsightPulse from '../../assets/InsightPulse.png';
+import Weather from '../../assets/Weather.png';
+import ProjectCard from '../../common/ProjectCard';
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer} >
-            <a href='https://github.com/GeeekySouro003/ChatNet' target='_blank'>
-            <img className='hover' src={ChatNet} alt='ChatNet logo' />
-            <h2>ChatNet</h2>
-            <p>Real Time Chat App</p>
-        </a>
+         <ProjectCard src={ChatNet} link='https://github.com/GeeekySouro003/ChatNet' h2='ChatNet' p='Real Time Communication App'/>
+       
+         <ProjectCard src={InsightPulse} link='https://github.com/GeeekySouro003/InsightPulse' h2='InsightPulse' p='Customized Feedback System'/>
+        
+         <ProjectCard src={Weather} link='https://github.com/GeeekySouro003/Weather-App' h2='Weather-App' p='Weather Forecasting App'/>
         </div>
     </section>
   )
